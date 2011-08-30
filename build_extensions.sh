2a46a3f93a44a13fdd1ac54fbd939ece90a1a6b4
Tyler Kennedy <tk@tkte.ch>
2011-08-30 16:54:37 -0400

    Updated build script

--- build_extensions.sh
+++ build_extensions.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
+
+# Build the core and copy it over
 coffee --bare --compile psychoxpath.coffee
 cp psychoxpath.js chrome
+
+# Build the individual components
+coffee --bare -o chrome/ -c chrome/
